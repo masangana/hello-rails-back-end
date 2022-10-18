93dcdd123de4bc24d45d5a05db59804b101c732f
masangana <alexmasangana@gmail.com>
2022-10-19 02:20:01 +0300

    update the controller
    Try to simplify the

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::MessagesController < ApplicationController
     def index
         messages = Message.all.order(created_at: :desc)
-        render json: {status: 'SUCCESS', message: 'Loaded messages', data: messages}, status: :ok
+        #render json: {status: 'SUCCESS', message: 'Loaded messages', data: messages}, status: :ok
+        render json: messages
     end
     
 end
