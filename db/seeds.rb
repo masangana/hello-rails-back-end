16648b1198796e58df68d029c0757745a331e6c9
masangana <alexmasangana@gmail.com>
2022-10-18 19:37:44 +0300

    Seed & migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+p "Seeding data..."
+p "Creating messages..."
+
+Message.create(greeting: "Hello Brother")
+Message.create(greeting: "Queen of north !")
+Message.create(greeting: "Khaleesi, Mère des Dragons, Reine de Meeren, Daenerys du Typhon, l'Imbrûlée, Mhysa, Briseuse de chaînes!")
+Message.create(greeting: "i'm james james bond")
+Message.create(greeting: "How are you?")
+
+p "Seeding data... DONE!"
+p "Created #{Message.count} messages!"
